b054df615044a1a86ae39ea6aa379e7b5608a9da
Joaquim Brasiliano Gonçalves <64588503+JbJet@users.noreply.github.com>
2025-05-07 17:25:01 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,16 @@
-import socket, asyncio
-async def server(host = 'localhost', port=8080):
-    data_payload = 2048 #The maximum amount of data to be received at once
-    # Create a TCP socket
-    sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-    # Enable reuse address/port 
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # Bind the socket to the port
-    server_address = (host, port)
-    print ("Starting up echo server  on %s port %s" % server_address)
-    sock.bind(server_address)
-    # Listen to clients, argument specifies the max no. of queued connections
-    sock.listen(5) 
-    i = 0
-    while True: 
-        print ("Client connected")
-        client, address = sock.accept() 
-        data = client.recv(data_payload) 
-        if data:
-            print ("Mensagem enviada: %s" %data)
-            client.send(data)
-            print ("Mensagem %s enviada pra to %s" % (data, address))
-            # end connection
-            client.close()       
-server()
+
+import socket
+ip = socket.gethostbyname(socket.gethostname())
+address = (ip, 55555)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(address)
+server.listen(5)
+print("Ligado", ip)
+
+while True:
+    client, add = server.accept()
+    msg = client.recv(1024).decode("utf-8")
+    if msg:
+        print(f"{msg} recebida")
+        client.close()
+        
